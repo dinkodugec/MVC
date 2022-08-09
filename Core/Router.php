6f98316dcc88b03a8bef698b55889138fdaab199
Dinko <dinko.dugec@gmail.com>
2022-08-09 19:39:47 +0200

    Create the routing table in the router, and add some routes

--- Core/Router.php
+++ Core/Router.php
@@ -10,4 +10,33 @@
 class Router
 {
 
+    /**
+     * Associative array of routes (the routing table)
+     * @var array
+     */
+    protected $routes = [];  //routing table, whic is ass array
+
+    /**
+     * Add a route to the routing table
+     *
+     * @param string $route  The route URL
+     * @param array  $params Parameters (controller, action, etc.)
+     *
+     * @return void
+     */
+    public function add($route, $params)  //$params - list of parametars;controllers, action etc
+    {
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * Get all the routes from the routing table
+     *
+     * @return array
+     */
+    public function getRoutes() //geting routing table
+    {
+        return $this->routes;
+    }
 }
+
--- public/index.php
+++ public/index.php
@@ -14,4 +14,19 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-echo get_class($router);
+//echo get_class($router);  
+/* The get_class() function gets the name of the class of an object. It returns FALSE if object is not an object. If object is excluded when inside a class,
+ the name of that class is returned */
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);  //home page
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+    
+// Display the routing table
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
+
+
+
