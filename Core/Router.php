facabba68468ed8047977b825d1f0dd339271acf
Dinko <dinko.dugec@gmail.com>
2022-08-09 23:03:08 +0200

    extracting controller and action from url using regular expression

--- Core/Router.php
+++ Core/Router.php
@@ -49,14 +49,31 @@ class Router
      */
     public function match($url)
     {
-        foreach ($this->routes as $route => $params) {
+         /*    foreach ($this->routes as $route => $params) {
             if ($url == $route) {    //very simple string comparision, if the route matching route in routing table
                 $this->params = $params;
                 return true;
             }
-        }
+        }  */
 
-        return false;
+      // Match to the fixed URL format /controller/action
+       $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+
+       if (preg_match($reg_exp, $url, $matches)) {
+           // Get named capture group values
+           $params = [];
+
+           foreach ($matches as $key => $match) {
+               if (is_string($key)) {
+                   $params[$key] = $match;
+               }
+           }
+
+           $this->params = $params;
+           return true;
+       } 
+
+       return false;
     }
 
     /**
