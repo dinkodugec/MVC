69094aa8344502153bc55ec94d22e2129228008a
Dinko <dinko.dugec@gmail.com>
2022-08-16 23:09:51 +0200

    add a base template to inherit from so remove repetion

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use \Core\View;
+
 /**
  * Posts controller
  */
@@ -15,9 +17,10 @@ class Posts extends \Core\Controller
      */
     public function indexAction()
     {
-      echo 'Hello from the index action in the Posts controller!';
+     /*  echo 'Hello from the index action in the Posts controller!';
       echo '<p>Query string parameters: <pre>' .
-           htmlspecialchars(print_r($_GET, true)) . '</pre></p>';
+           htmlspecialchars(print_r($_GET, true)) . '</pre></p>'; */
+           View::renderTemplate('Posts/index.html');
     }
 
     /**
