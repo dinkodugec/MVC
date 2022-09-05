044dd6f2504b85c404cc859a7eceb21c232dbd3e
Dinko <dinko.dugec@gmail.com>
2022-09-05 22:58:35 +0200

    restrict a page to logged-in user only

---
+++ App/Controllers/Items.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Auth;
+
+/**
+ * Items controller (example)
+ */
+class Items extends \Core\Controller
+{
+
+    /**
+     * Items index
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+        if (! Auth::isLoggedIn()) {
+            $this->redirect('/public/index.php?login');
+        }
+
+        View::renderTemplate('Items/index.html');
+    }
+}
