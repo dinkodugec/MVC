eedc22266a035d25b31dc4fac9d1cb6b4edfb517
Dinko <dinko.dugec@gmail.com>
2022-11-29 23:12:49 +0100

    get email content from a view template

--- Core/View.php
+++ Core/View.php
@@ -29,7 +29,7 @@ class View
         }
     }
 
-     /**
+      /**
      * Render a view template using Twig
      *
      * @param string $template  The template file
@@ -38,6 +38,23 @@ class View
      * @return void
      */
     public static function renderTemplate($template, $args = [])
+    {
+        echo static::getTemplate($template, $args);    
+    } 
+
+
+        
+
+
+     /**
+     * Render a view template using Twig
+     *
+     * @param string $template  The template file
+     * @param array $args  Associative array of data to display in the view (optional)
+     *
+     * @return void
+     */
+    public static function getTemplate($template, $args = [])
     {
         static $twig = null;
 
@@ -55,6 +72,6 @@ class View
             $twig->addExtension(new \Twig\Extension\DebugExtension());
         }
 
-        echo $twig->render($template, $args);
+        return $twig->render($template, $args);
     } 
 }
